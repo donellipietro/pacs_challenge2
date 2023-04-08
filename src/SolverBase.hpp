1d72211caec48b12aa7fbd6cc61d0524fb0f569b
Pietro Donelli <pietro.donelli99@gmail.com>
2023-04-08 14:59:03 +0000

    SolverBase and traits

---
+++ src/SolverBase.hpp
@@ -0,0 +1,33 @@
+#ifndef __SOLVER_BASE__
+#define __SOLVER_BASE__
+
+#include "SolverTraits.hpp"
+
+class SolverBase {
+
+    public:
+        using T = SolverTraits;
+    
+    protected:
+        T::FunctionType f_;
+        T::ScalarType tol_;
+        unsigned int maxIter_;
+
+    public:
+        
+        SolverBase() = default;
+        SolverBase(T::FunctionType f,
+                   T::ScalarType tol = 1e-4,
+                   unsigned int maxIter = 150) :
+                   f_(f), tol_(tol), maxIter_(maxIter) {}
+
+        // setters
+        void setFunction(T::FunctionType f) { f_ = f; };
+        void setTollerance(T::ScalarType tol) { tol_ = tol; };
+        void setMaxIter(unsigned int maxIter) { maxIter_ = maxIter; };
+
+        virtual T::ReturnType solve() = 0;
+
+};
+
+#endif // __SOLVER_BASE__
---
+++ src/SolverTraits.hpp
@@ -0,0 +1,13 @@
+#ifndef __SOLVER_TRAITS__
+#define __SOLVER_TRAITS__
+
+#include <functional>
+
+struct SolverTraits{
+    using ScalarType = double;
+    using VariableType = double;
+    using ReturnType = double;
+    using FunctionType = std::function<ReturnType(const VariableType &)>;
+};
+
+#endif // __SOLVER_TRAITS__
