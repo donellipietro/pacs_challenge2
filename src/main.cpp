80f22ed549bc525af2a79c95379eb2575f17740d
Pietro Donelli <pietro.donelli99@gmail.com>
2023-04-26 21:23:32 +0000

    main

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 
 #include <cmath>
 #include "SolverFactory.hpp"
@@ -13,15 +14,39 @@ int main(int argc, char **argv)
         [](const double x)
         { return -M_PI * std::exp(M_PI * x); }};
 
-    auto solver1 = SolverFactory<Secant>(f, -1., 0);
-    std::cout << solver1->solve() << std::endl;
-
     std::array<SolverTraits::VariableType, 2> interval{-1., 0.};
-    auto solver2 = SolverFactory<Bisection>(f, interval);
-    std::cout << solver2->solve() << std::endl;
 
+    std::cout << std::endl;
+    std::cout << "###########" << std::endl;
+    std::cout << "# Solvers #" << std::endl;
+    std::cout << "###########" << std::endl;
+    std::cout << std::endl;
+
+    std::cout << "Function: 0.5 - exp{pi*x}" << std::endl;
+    std::cout << "Expected zero: " << std::log(0.5) / M_PI << std::endl;
+    std::cout << std::endl;
+
+    // Secant
+    auto solver1 = SolverFactory<Secant>(f, interval);
+    auto result1 = SafeSolve(solver1);
+    std::cout << std::setw(15) << std::left << "- Secant: " << result1 << std::endl;
+
+    // Bisection
+    auto solver2 = SolverFactory<Bisection>(f, interval);
+    auto result2 = SafeSolve(solver2);
+    std::cout << std::setw(15) << std::left << "- Bisection: " << result2 << std::endl;
+
+    // Newton
     auto solver3 = SolverFactory<Newton>(f, df, -1.);
-    std::cout << solver3->solve() << std::endl;
+    auto result3 = SafeSolve(solver3);
+    std::cout << std::setw(15) << std::left << "- Newton: " << result3 << std::endl;
+
+    // QuasiNewton
+    auto solver4 = SolverFactory<Newton>(f, df, -1.);
+    auto result4 = SafeSolve(solver4);
+    std::cout << std::setw(15) << std::left << "- QuasiNewton: " << result4 << std::endl;
+
+    std::cout << std::endl;
 
     return 0;
 }
